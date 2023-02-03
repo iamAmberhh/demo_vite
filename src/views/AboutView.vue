cb502cc2102591a445f2f48d91d0dc64253000dc
iamAmberhh <melody19941231@gmail.com>
2023-02-03 14:56:41 +0800

    second commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ data }}
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      data: 123,
+    };
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEST);
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) => (this.data = res.data.results[0]))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
